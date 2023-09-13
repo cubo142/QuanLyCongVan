87a0eb42d76fdfe1d7d0e07e91a8bdabb16a0ebd
cubo142 <doducminh8694@gmail.com>
2023-09-13 10:43:15 +0700

    update code appjs

--- frontend/quanlycongvan-project/src/App.js
+++ frontend/quanlycongvan-project/src/App.js
@@ -2,7 +2,6 @@ import Layout from "./components/global/Layout";
 import PageTrangChu from './pages/PageTrangChu';
 import { Routes, Route } from "react-router-dom";
 import QLNhanVien from "./components/quanly/QLNhanVien/QLNhanVien";
-import PageQuanLy from "./pages/PageQuanLy";
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<PageTrangChu />} />
-          <Route path="quanly" element={<PageQuanLy />} />
+          <Route path="quanly" element={<QLNhanVien />} />
         </Routes>
       </Layout>
 
