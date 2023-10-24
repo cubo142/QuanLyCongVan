17ad5235a7672f3fd64d9d93d2ea26912c54f6a0
DucMinh <doducminh8694@gmail.com>
2023-10-24 16:33:33 +0700

    update api

--- frontend/quanlycongvan-project/src/App.js
+++ frontend/quanlycongvan-project/src/App.js
@@ -4,17 +4,22 @@ import PageQuanLy from './pages/PageQuanLy';
 import PagePhongBan from './pages/PagePhongBan';
 import { Routes, Route } from "react-router-dom";
 import PageCongVan from "./pages/PageCongVan";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({});
 
 function App() {
   return (
     <>
       <Layout>
-        <Routes>
-          <Route path="/" element={<PageTrangChu />} />
-          <Route path="quanly" element={<PageQuanLy />} />
-          <Route path="phongban" element={<PagePhongBan />} />
-          <Route path="congvan" element={<PageCongVan />} />
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<PageTrangChu />} />
+            <Route path="quanly" element={<PageQuanLy />} />
+            <Route path="phongban" element={<PagePhongBan />} />
+            <Route path="congvan" element={<PageCongVan />} />
+          </Routes>
+        </QueryClientProvider>
       </Layout>
 
     </>
